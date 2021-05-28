68675aafc17ffa06dbe21ddd5117d4f95a163a69
Hilman Giri P <hilmangp@gmail.com>
2021-05-28 07:33:31 +0700

    add Middleware for authentication

---
+++ config/firebase.ts
@@ -0,0 +1,11 @@
+let firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.uthDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+};
+
+export default firebaseConfig;
---
+++ middleware/firebase.ts
@@ -0,0 +1,7 @@
+
+import firebase from "firebase/app";
+import firebaseConfig from "../config/firebase";
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
